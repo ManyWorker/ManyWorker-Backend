67592c6e93fd81822231d0b436d0e397c486f012
anavarj1510 <anavarj1510@g.educaand.es>
2025-11-13 11:00:48 +0100

    Modificacion cliente para permitir la exportación y eliminación de datos(GDPR)  y permitir la edición de datos personales.

--- src/main/java/manyWorker/repository/ClienteRepository.java
+++ src/main/java/manyWorker/repository/ClienteRepository.java
@@ -1,9 +1,11 @@
 package manyWorker.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import manyWorker.entity.Cliente;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-
+	
 }
